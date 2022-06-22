25150203fe38b59b4e9a1b799db028a2b23107e5
Merve Gulle <glle0001@umn.edu>
2022-06-22 18:47:22 -0500

    perf: Shuffle the training data at every epoch

--- SupportingFunctions.py
+++ SupportingFunctions.py
@@ -82,7 +82,7 @@ def prepare_train_loaders(dataset,params,g):
 
     train_loader = DataLoader(dataset       = train_dataset,
                             batch_size      = params['batch_size'],
-                            shuffle         = False,
+                            shuffle         = True,
                             drop_last       = True,
                             #worker_init_fn  = seed_worker,
                             num_workers     = params['num_workers'],
@@ -90,7 +90,7 @@ def prepare_train_loaders(dataset,params,g):
 
     valid_loader = DataLoader(dataset       = valid_dataset,
                             batch_size      = params['batch_size'],
-                            shuffle         = False,
+                            shuffle         = True,
                             drop_last       = True,
                             #worker_init_fn  = seed_worker,
                             num_workers     = params['num_workers'],
@@ -98,7 +98,7 @@ def prepare_train_loaders(dataset,params,g):
 
     full_loader= DataLoader(dataset         = dataset,
                             batch_size      = params['batch_size'],
-                            shuffle         = False,
+                            shuffle         = True,
                             drop_last       = False,
                             #worker_init_fn  = seed_worker,
                             num_workers     = params['num_workers'],
